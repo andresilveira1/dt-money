ef3ce7623e85bd724e491960bcf1497ea0acd737
André Silveira <andre.silveira1silva@gmail.com>
2024-07-24 18:52:19 -0300

    feat: new transaction button updated

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,22 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
 import { Content, HeaderContainer, NewTransactionButton } from './styles'
 import logo from '../../assets/logo.svg'
+import { NewTransactionModal } from '../new-transaction-modal'
 
 export function Header() {
   return (
     <HeaderContainer>
       <Content>
         <img src={logo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </Content>
     </HeaderContainer>
   )
